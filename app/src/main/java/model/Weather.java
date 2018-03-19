350253e895320dc56eb50cf2a4d8c9545947099a
mahbub <mahbub665@gmail.com>
2018-03-19 10:21:38 +0600

    instantiate object variable in weather class

--- app/src/main/java/model/Weather.java
+++ app/src/main/java/model/Weather.java
@@ -6,4 +6,12 @@ package model;
 
 public class Weather {
 
+    public Place place;
+    public String icon;
+    CurrentCondition currentCondition = new CurrentCondition();
+    Temperature temperature = new Temperature();
+    Wind wind = new Wind();
+    Snow snow = new Snow();
+    Clouds clouds = new Clouds();
+
 }
