97836fcb782d039947b4a516321741300520db2d
mahbub <mahbub665@gmail.com>
2018-03-19 09:31:08 +0600

    add Clouds.java file and declare variable and method

---
+++ app/src/main/java/model/Clouds.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Created by mahbub on 3/19/2018.
+ */
+
+public class Clouds {
+    private int precipitation;
+
+    public int getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(int precipitation) {
+        this.precipitation = precipitation;
+    }
+}
