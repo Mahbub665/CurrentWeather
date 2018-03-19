f03a2a1ef7a2754641d711170e24db9722833e44
mahbub <mahbub665@gmail.com>
2018-03-19 09:56:49 +0600

    add CurrentCondition.java model class and declare variable and method

---
+++ app/src/main/java/model/CurrentCondition.java
@@ -0,0 +1,90 @@
+package model;
+
+/**
+ * Created by mahbub on 3/19/2018.
+ */
+
+public class CurrentCondition {
+    private int weatherId;
+    private String condition;
+    private String description;
+    private String icon;
+    private double temperature;
+    private float pressure;
+    private float humidity;
+    private float temp_min;
+    private float temp_max;
+
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(float pressure) {
+        this.pressure = pressure;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+
+    public float getTemp_min() {
+        return temp_min;
+    }
+
+    public void setTemp_min(float temp_min) {
+        this.temp_min = temp_min;
+    }
+
+    public float getTemp_max() {
+        return temp_max;
+    }
+
+    public void setTemp_max(float temp_max) {
+        this.temp_max = temp_max;
+    }
+}
