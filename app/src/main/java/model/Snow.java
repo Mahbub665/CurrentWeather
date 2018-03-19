2efefecb029224cab95e4fe564c6ada3560f94bf
mahbub <mahbub665@gmail.com>
2018-03-19 10:03:13 +0600

    add Snow.java model class and declare method

---
+++ app/src/main/java/model/Snow.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Created by mahbub on 3/19/2018.
+ */
+
+public class Snow {
+    private int precipitation;
+
+    public int getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(int precipitation) {
+        this.precipitation = precipitation;
+    }
+}
